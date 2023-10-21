d40158d2f3c5cfac3bdd31e4a589b1916a433da2
Д. М <skider96@abv.bg>
2023-10-21 18:37:41 +0300

    Add project files.

---
+++ 01.SoftUniReception/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01.SoftUniReception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployeeEfficency = int.Parse(Console.ReadLine());
+            int secondEmployeeEfficency = int.Parse(Console.ReadLine());
+            int thirdEmployeeEfficency = int.Parse(Console.ReadLine());
+            int studentsCount = int.Parse(Console.ReadLine());
+            int hour = 1;
+
+            int fullEfficency = firstEmployeeEfficency + secondEmployeeEfficency + thirdEmployeeEfficency;
+
+            for (int i = 1; i <= int.MaxValue; i++)
+            {
+                studentsCount -= fullEfficency;
+                if (hour % 4 == 0)
+                {
+                    hour += 1;
+                }
+                if (studentsCount <= 0)
+                {
+
+                    Console.WriteLine($"Time needed: {hour}h.");
+                    return;
+                }
+                hour++;
+            }
+        }
+    }
+}
---
+++ 03.Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _03.Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
